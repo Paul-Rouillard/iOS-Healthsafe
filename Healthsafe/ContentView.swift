3f9ccac839555a2100cf6d501d892197ad89eb66
Paul Rouillard <paul.rouillard@epitech.eu>
2020-07-05 18:05:26 +0200

    Initial Commit

---
+++ Healthsafe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Healthsafe
+//
+//  Created by Paul Rouillard on 05/07/2020.
+//  Copyright © 2020 Healthsafe. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
